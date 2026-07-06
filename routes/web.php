b7873b8bd0e164346b76241937140e241cf426b7
mdrzzz24 <achmadrozan24@gmail.com>
2026-07-06 09:38:17 +0700

    Migrate Coming Soon page to Laravel

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+Route::get('/home1', function () {
+    return view('home1');
+});
 
 Route::get('/home2', function () {
     return view('home2');
